6899b71f42da78e031e8f464d8b8e4a0f67c7746
Lugon LQ <48004924+lugondev@users.noreply.github.com>
2022-07-21 16:27:20 +0700

    BamToken

--- contracts/BamUSD.sol
+++ contracts/BamUSD.sol
@@ -2,19 +2,12 @@
 
 pragma solidity ^0.8.3;
 
-import "./BERC20Snapshot.sol";
-import "./interfaces/IAM.sol";
-import "./interfaces/IBridgeConverter.sol";
-import "./interfaces/IMigration.sol";
+import "./BToken.sol";
 
-contract BamUSD is BERC20Snapshot {
-    event Migration(address indexed account, uint256 amount);
+contract BamUSD is BToken {
     event UpdateBridge(address indexed account, bool status);
     event Bridge(address indexed from, address indexed to, uint256 amount);
 
-    IAM public iam;
-    IMigration public migration;
-    mapping(address => bool) public migrated;
     mapping(address => bool) public bridges;
 
     function initialize(
@@ -22,47 +15,12 @@ contract BamUSD is BERC20Snapshot {
         string memory _symbol,
         address _iam
     ) public initializer {
-        __BERC20_init(_name, _symbol);
-
-        iam = IAM(_iam);
-    }
-
-    modifier rejectBlacklist() {
-        require(
-            !iam.isBlacklist(address(this), msg.sender),
-            "you are in blacklist"
-        );
-        _;
+        __BToken_init(_name, _symbol, _iam);
     }
 
     modifier onlyBridge() {
         require(bridges[msg.sender], "you are not a bridge");
         _;
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override rejectBlacklist whenNotPaused {
-        require(address(migration) == address(0), "time to migrate");
-        super._beforeTokenTransfer(from, to, amount);
-    }
-
-    function updateIAM(address _iam) public onlyOwner {
-        iam = IAM(_iam);
-    }
-
-    function mint(address _address, uint256 _amount) public onlyOwner {
-        _mint(_address, _amount);
-    }
-
-    function snapshot() public onlyOwner {
-        _snapshot();
-    }
-
-    function burn(uint256 _amount) public {
-        _burn(_msgSender(), _amount);
     }
 
     function bridge(
@@ -73,26 +31,6 @@ contract BamUSD is BERC20Snapshot {
         _forceTransfer(_user, _to, _amount);
 
         emit Bridge(_user, _to, _amount);
-    }
-
-    function getCurrentSnapshotId() public view returns (uint256) {
-        return _getCurrentSnapshotId();
-    }
-
-    function setMigration(address _migration) public onlyOwner {
-        require(_migration != address(0), "invalid address");
-        require(address(migration) != address(0), "only migrate once time");
-        migration = IMigration(_migration);
-    }
-
-    function migrate(uint256 _amount) public {
-        require(address(migration) != address(0), "not time to migrate");
-        require(!migrated[_msgSender()], "you migrated");
-
-        migrated[_msgSender()] = true;
-        migration.migrate(_msgSender(), _amount);
-
-        emit Migration(_msgSender(), _amount);
     }
 
     function addBridge(address _bridge) public onlyOwner {
