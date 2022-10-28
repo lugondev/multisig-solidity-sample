46d75dc094d4b09755da0fee89f31b2fdcb113b7
Lugon LQ <48004924+lugondev@users.noreply.github.com>
2022-10-28 13:57:02 +0700

    multisig

--- contracts/interfaces/IBridgeConverter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.3;
-
-interface IBridgeConverter {
-    function convert(uint256 _amount) external;
-
-    function release(address _address, uint256 _amount) external;
-}
